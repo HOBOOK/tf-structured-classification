9518deb38186b2d5ccce7f97483a89d780bc88d3
peter <pkh879@gmail.com>
2022-06-30 11:37:06 +0900

    [ADD] 프로젝트 생성

---
+++ tf.py
@@ -0,0 +1,20 @@
+import numpy as np
+import pandas as pd
+
+import tensorflow as tf
+
+from tensorflow import feature_column
+from tensorflow.keras import layers
+from sklearn.model_selection import train_test_split
+
+URL = 'https://storage.googleapis.com/applied-dl/heart.csv'
+dataframe = pd.read_csv(URL)
+dataframe.head()
+
+train, test = train_test_split(dataframe, test_size=0.2)
+train, val = train_test_split(train, test_size=0.2)
+print(len(train), '훈련 샘플')
+print(len(val), '검증 샘플')
+print(len(test), '테스트 샘플')
+
+  
